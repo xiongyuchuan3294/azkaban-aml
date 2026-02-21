bd2015a808de135c5ebcbc377fe3a91f6e211eb7
xiongyuchuan <xiongyuchuan@xiaomi.com>
2026-02-21 10:41:33 +0800

    ceshi

--- workspace/azkaban-aml/sql/03_dwd_to_dws.sql
+++ workspace/azkaban-aml/sql/03_dwd_to_dws.sql
@@ -3,7 +3,7 @@ use aml_demo;
 insert overwrite table dws_acct_risk_stat_di partition (dt='${hivevar:run_date_std}')
 select
   acct_id,
-  count(1) as txn_cnt,
+  count(*) as txn_cnt,
   cast(sum(amount) as decimal(18,2)) as total_amt,
   sum(night_flag) as night_txn_cnt,
   sum(large_flag) as large_txn_cnt,
